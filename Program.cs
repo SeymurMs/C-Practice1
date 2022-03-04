590e37157eaeab85e6659345274933e4af7ce87a
Akif Guliyev <tu5zlmzfj@aztu.local>
2022-03-04 17:09:04 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region switchCaseAylar
+            //dynamic month = "";
+
+            //switch (month)
+            //{
+
+            //    case "december":
+            //    case "january":
+            //    case "fevruary":
+            //        Console.WriteLine("Winter");
+            //        break;    
+            //    case "march":
+            //    case "april":
+            //    case "may":
+            //        Console.WriteLine("spring");
+            //        break;
+            //    case "june":
+            //    case "july":
+            //    case "august":
+            //        Console.WriteLine("summer");
+            //        break;
+            //    case "september":
+            //    case "november":
+            //    case "october":
+            //        Console.WriteLine("autumn");
+            //        break;
+            //    default:
+            //        Console.WriteLine("wrong input");
+            //        break;
+
+            //}
+            #endregion
+
+
+            //#region faktorial
+            //int num = 5;
+            //int fact = 1;
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    fact = fact * i;
+            //}
+            //Console.WriteLine(fact);
+            //#endregion 
+            #region musbet ededin mertebeleri
+            //int count = 0;
+
+            //while (true)
+            //{
+
+            //    Console.WriteLine("input:");
+            //    int a = Convert.ToInt32(Console.ReadLine());
+            //    if (a < 0)
+            //    {
+            //       continue;
+
+
+                    
+            //    }
+            //    while (a >= 1)
+            //    {
+
+            //        a /= 10;
+            //        count++;
+
+                    
+            //    }
+            //    Console.WriteLine(count);
+            //    break;
+            //    #endregion
+
+
+
+
+            }
+        }
+
+    }
